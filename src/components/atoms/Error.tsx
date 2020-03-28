3a2effa5d0c325eaec060cb531d06db880473c65
wtae1216 <wtae1216@gmail.com>
2020-03-29 02:27:52 +0900

    emotion 테스트를 위한 템플릿 추가  - Nanum Gothic 폰트 추가  - Error 컴포넌트에 스타일 추가

--- src/components/atoms/Error.tsx
+++ src/components/atoms/Error.tsx
@@ -1,11 +1,19 @@
-import React, { FC } from 'react';
+/** @jsx jsx */
+import { FC } from 'react';
+
+import { css, jsx } from '@emotion/core';
 
 interface IError {
     /** 출력할 메시지 */
     message: string;
 }
 
+const ErrorStyle = css`
+    width: 100%;
+    text-align: center;
+`;
+
 /** 에러 메시지 출력용 컴포넌트 */
-const Error: FC<IError> = ({ message }) => <p>{message}</p>;
+const Error: FC<IError> = ({ message }) => <p css={ErrorStyle}>{message}</p>;
 
 export default Error;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import React from 'react';
 import { END } from 'redux-saga';
 
+import { css, Global } from '@emotion/core';
+
 import axios from 'libs/axios';
 
 import TStoreState from 'types/TStoreState';
@@ -11,6 +13,58 @@ import TStoreState from 'types/TStoreState';
 import { initStore } from 'modules/store';
 
 import { sagaTask } from 'sagas';
+
+const GlobalStyle = css`
+    @font-face {
+        font-family: 'Nanum Gothic';
+        font-style: normal;
+        font-weight: 400;
+        src: url('/fonts/Nanum-Gothic.eot');
+        src: local('NanumGothic'),
+            url('/fonts/Nanum-Gothic.eot?#iefix') format('embedded-opentype'),
+            url('/fonts/Nanum-Gothic.woff2') format('woff2'),
+            url('/fonts/Nanum-Gothic.woff') format('woff'),
+            url('/fonts/Nanum-Gothic.ttf') format('truetype'),
+            url('/fonts/Nanum-Gothic.svg#NanumGothic') format('svg');
+    }
+
+    @font-face {
+        font-family: 'Nanum Gothic';
+        font-style: normal;
+        font-weight: 800;
+        src: url('/fonts/Nanum-Gothic-Extra-Bold.eot');
+        src: local('NanumGothic ExtraBold'), local('NanumGothic-ExtraBold'),
+            url('/fonts/Nanum-Gothic-Extra-Bold.eot?#iefix')
+                format('embedded-opentype'),
+            url('/fonts/Nanum-Gothic-Extra-Bold.woff2') format('woff2'),
+            url('/fonts/Nanum-Gothic-Extra-Bold.woff') format('woff'),
+            url('/fonts/Nanum-Gothic-Extra-Bold.ttf') format('truetype'),
+            url('/fonts/Nanum-Gothic-Extra-Bold.svg#NanumGothic') format('svg');
+    }
+
+    @font-face {
+        font-family: 'Nanum Gothic';
+        font-style: normal;
+        font-weight: 700;
+        src: url('/fonts/Nanum-Gothic-Bold.eot');
+        src: local('NanumGothic Bold'), local('NanumGothic-Bold'),
+            url('/fonts/Nanum-Gothic-Bold.eot?#iefix')
+                format('embedded-opentype'),
+            url('/fonts/Nanum-Gothic-Bold.woff2') format('woff2'),
+            url('/fonts/Nanum-Gothic-Bold.woff') format('woff'),
+            url('/fonts/Nanum-Gothic-Bold.ttf') format('truetype'),
+            url('/fonts/Nanum-Gothic-Bold.svg#NanumGothic') format('svg');
+    }
+
+    html {
+        font-size: 10px;
+        font-family: 'Nanum Gothic', serif;
+    }
+
+    body {
+        font-size: 1.4rem;
+    }
+`;
 
 class App extends RootApp<ReduxWrapperAppProps<TStoreState>> {
     static async getInitialProps({ Component, ctx }: AppContext) {
@@ -39,6 +93,7 @@ class App extends RootApp<ReduxWrapperAppProps<TStoreState>> {
         const { Component, pageProps, store } = this.props;
         return (
             <Provider store={store}>
+                <Global styles={GlobalStyle} />
                 <Component {...pageProps} />
             </Provider>
         );
