d2dae0cfd0968512759035a04b44376e9db681aa
wtae1216 <wtae1216@gmail.com>
2020-03-29 02:38:23 +0900

    한 파일에 css 선언 시 import > css > component 순으로 작성

--- src/components/atoms/Error.tsx
+++ src/components/atoms/Error.tsx
@@ -3,15 +3,15 @@ import { FC } from 'react';
 
 import { css, jsx } from '@emotion/core';
 
-interface IError {
-    /** 출력할 메시지 */
-    message: string;
-}
-
 const ErrorStyle = css`
     width: 100%;
     text-align: center;
 `;
+
+interface IError {
+    /** 출력할 메시지 */
+    message: string;
+}
 
 /** 에러 메시지 출력용 컴포넌트 */
 const Error: FC<IError> = ({ message }) => <p css={ErrorStyle}>{message}</p>;
