4f070c5f506fd3ff0df7870035c417feec8bf155
wtae1216 <wtae1216@gmail.com>
2020-04-07 02:45:21 +0900

    테스트를 위한 custom library 수정 및 추가  - axios mock 을 위한 library 설정 추가  - tests-utils.tsx : 테스트를 위한 util 함수 추가

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,4 +1,5 @@
 import originalAxios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 
 const axios = originalAxios.create();
 
@@ -8,5 +9,8 @@ axios.defaults.headers = {
     'Content-Type': 'x-www-form-urlencoded', // default content type
     'Access-Control-Allow-Origin': '*'
 };
+
+// axios-mock-adapter
+export const axiosMock = (options = {}) => new MockAdapter(axios, options);
 
 export default axios;
