2378ec46aa8768966b462b5d6ae4e9323b8bb29f
wtae1216 <wtae1216@gmail.com>
2020-05-01 15:52:36 +0900

    svg 파일 로더 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,12 @@ import { NextPage } from 'next';
 
 import GnbLayout from 'components/templates/GnbLayout';
 
+import Logo from 'svg/logo.svg';
+
 const Home: NextPage = () => {
     return (
         <GnbLayout title={'Home'}>
+            <Logo width="40px" height="40px" />
             <h1>Hello, Next Ts</h1>
         </GnbLayout>
     );
