9807f889eca20ada4c0af559b7c6dcccf1c04228
wtae1216 <wtae1216@gmail.com>
2020-05-05 20:40:16 +0900

    storybook config 수정  - storybook 에서 svg 적용  - 테스트를 위한 Button component 제작 및 storybook/test 적용

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,13 +1,16 @@
 // in config.js
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Global } from '@emotion/core';
 import { addDecorator } from '@storybook/react';
+
+import { initStore } from 'modules/store';
 
 import { GlobalStyle } from 'styles/App.style';
 
 addDecorator((context) => (
-    <>
+    <Provider store={initStore()}>
         <Global styles={GlobalStyle} />
         {context()}
-    </>
+    </Provider>
 ));
